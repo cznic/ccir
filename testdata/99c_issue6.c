b9fa025f55565a9a1e0e29ca8c4a97d4e290c5be
Jan Mercl <0xjnml@gmail.com>
2017-10-12 22:37:41 +0200

    Fix last commit.
    	modified:   testdata/99c_issue6.c

--- testdata/99c_issue6.c
+++ testdata/99c_issue6.c
@@ -8,12 +8,23 @@ struct Node {
   int count;
 };
 
+typedef struct NodePool {
+  Node* next;
+} NodePool;
+
 void foo(void*) {}
 
-int main() { // https://github.com/cznic/99c/issues/5
+static void BoundaryPM(Node* (*lists)[2]) {
+  foo(lists);
+  foo(lists[0]); //TODO
+  foo(lists[0][0]); //TODO
+}
+
+int main() { // https://github.com/cznic/99c/issues/6
   Node* (*lists)[2];
   lists = (Node* (*)[2])malloc(42 * sizeof(*lists));
   foo(lists);
   foo(lists[0]);
+  foo(lists[0][0]);
   __builtin_printf("ok\n");
 }
